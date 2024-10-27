bae66a6443c063f95c603665266dd01a7914b536
AmmarAdnew <132451928+AmmarAdnew@users.noreply.github.com>
2024-10-27 01:06:11 +0000

    feature/email sent notify done

--- FrontEnd/src/components/ContactUs.jsx
+++ FrontEnd/src/components/ContactUs.jsx
@@ -1,28 +1,68 @@
 import { FaTelegramPlane } from "react-icons/fa";
 import contactUsImg from "../assets/image/Contact us.gif";
+import React, { useRef, useState  } from 'react';
+import emailjs from '@emailjs/browser';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
+
 export default function ContactUs() {
+
+  const MySwal = withReactContent(Swal);
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_36b7k99', 'template_9ich0xz', form.current, {
+        publicKey: '5kmb4zPtSXgsmWyzz',
+      })
+      .then(
+        () => {
+          Swal.fire({
+            title: 'Message Sent!',
+            text: 'Thank you for contacting us. We will be in touch shortly.',
+            icon: 'success'
+          });
+          
+        },
+        (error) => {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Something went wrong!"
+          });
+          
+
+        },
+      );
+  };
+
   return (
     <div className="flex gap-10">
-      <div className="flex flex-col gap-5 flex-1">
+
+      <form ref={form} onSubmit={sendEmail} className="flex flex-col gap-5 flex-1">
         <div>
-          <label htmlFor="name" className="text-2xl text-gray-800">
+          <label htmlFor="name"  className="text-2xl text-gray-800">
             Name*
           </label>
           <input
-            type="text"
+            type="text" name="user_name"
             id="name"
-            placeholder="Enter your name"
+            
+            placeholder="Enter your name" required
             className="w-full border-[1px] p-4 mt-2 bg-primary  rounded-lg focus:outline-none"
           />
         </div>
         <div>
-          <label htmlFor="email" className="text-2xl text-gray-800">
+          <label htmlFor="email"  className="text-2xl text-gray-800">
             Email*
           </label>
           <input
-            type="email"
+            type="email" name="user_email"
             id="email"
-            placeholder="Enter your email"
+            placeholder="Enter your email" required
           className="w-full border-[1px] p-4 mt-2 bg-primary  rounded-lg focus:outline-none"
           />
         </div>
@@ -32,22 +72,25 @@ export default function ContactUs() {
           </label>
           <textarea
             id="message"
+            name="message"
             placeholder="Enter your message"
-            rows="8"
+            rows="8" required
             className="w-full p-4 mt-2 bg-primary border-[1px]  rounded-lg focus:outline-none"
           />
         </div>
 
         <div className="flex ml-auto">
-          <button className="bg-red-500 text-white text-xl py-2 px-6 flex gap-2 items-center shadow-[4px_6px_0px_0px_rgba(0,0,0,0.2)]">
+          <button type="submit" value="Send" className="bg-red-500 text-white text-xl py-2 px-6 flex gap-2 items-center shadow-[4px_6px_0px_0px_rgba(0,0,0,0.2)]">
             Send Message
             <FaTelegramPlane color="#fff" />
           </button>
         </div>
-      </div>
+      </form>
+      {/* <NotificationContainer />  */}
       <div className="flex-1 hidden lg:block">
         <img src={contactUsImg} alt="" />
       </div>
+      
     </div>
   );
 }
